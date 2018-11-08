4fb9cabef6924c1f073b8a672c9470a92faea779
nickjevershed <nick.evershed@gmail.com>
2018-11-09 10:48:52 +1100

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Classify points into Australian federal electorates
+
+A function that takes a csv of locations and checks which electorate they fall into, then adds the electorate name, margin from preceding election and incumbent party. The function takes two arguments, year and inputFile location. 
+
+The inputFile should have a row for each location, with a seperate column "lat" and "lon" for each.
+
+## Notes
+
+- Uses geoJSON files simplified to 10% using mapshaper, Visvalingam simplification with "prevent shape removal" selected.
+- Electoral divisions 2016 from the ABS: http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.003July%202016?OpenDocument
+- Electoral divisions 2013 from the AEC: https://www.aec.gov.au/Electorates/gis/gis_datadownload.htm
+- Margins and incumbent parties taken from the ABC's election pendulums.
---
+++ marginator.py
@@ -0,0 +1,46 @@
+import json
+from shapely.geometry import Point, shape
+import csv
+
+def assignMargin(year,inputFile):
+	
+	with open('{year}.json'.format(year=year)) as f:
+		js = json.load(f)
+
+	with open(inputFile,'r') as csvinput:
+		with open('output.csv', 'w') as csvoutput:
+
+			reader = csv.DictReader(csvinput, lineterminator='\n')
+			headers = reader.fieldnames
+			electorate = 'electorate-{year}'.format(year=year)
+			incumbent = 'incumbent-{year}'.format(year=year)
+			margin = 'margin-{year}'.format(year=year)
+			headers = headers + [electorate, incumbent, margin]
+			
+			writer = csv.DictWriter(csvoutput, headers, lineterminator='\n')
+			
+			newrows = []
+
+			for row in reader:
+				print("Finding", row['location'])
+				row[electorate] = 'na'
+				row[incumbent] = 'na'
+				row[margin] = 'na'
+				for area in js['features']:
+					print(area['properties']['name'])
+					if area['geometry']:
+						shapeGeo = shape(area['geometry'])
+						point = Point(float(row['lon']),float(row['lat']))
+						if shapeGeo.contains(point):
+							print(area['properties']['name'])
+							row[electorate] = area['properties']['name']
+							row[incumbent] = area['properties']['incumbent']
+							row[margin] = area['properties']['margin']
+
+				newrows.append(row)			
+
+			writer.writeheader()
+			writer.writerows(newrows)
+
+
+assignMargin('2016','input-2.csv')
